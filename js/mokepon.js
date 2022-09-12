938b1f119acdc8feb91577795e404e4d66d13505
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2022-09-11 22:15:16 -0500

    ajustes:adicionar al DOM una imagen en seccion de combate

--- js/mokepon.js
+++ js/mokepon.js
@@ -13,10 +13,10 @@ const imphipodo=document.getElementById('hipodo')
 const impcapi=document.getElementById('capi') 
 const imprat=document.getElementById('rat') 
 const spnmascotapl=document.getElementById('nommacotapl')
-
+const imgPl=document.getElementById('imgCombatiente')//agrgar imagem al dom
 // variables de la funcion selectMascotaPc
 const spnmascotapc = document.getElementById('nommacotapc')
-
+const imgpc=document.getElementById('imgCombatientePc')//agrgar imagem al dom
 //variables de la fincion combate()
 const spanVidaJugador =document.getElementById('vidapl')
 const spanVidaPc =document.getElementById('vidapc')
@@ -33,6 +33,7 @@ let ataqueJugador
 let ataqAleatorio
 let vidasJugador =3
 let vidaspc =3
+
 class Mokepon{
     //objetos instancia
     constructor(nombre,foto,vida){
@@ -93,23 +94,29 @@ function iniciarjuego(){
 }
 
 function selctMascotaPl(){
+    
+    
     sectionMascota.style.display ='none'
     sectionAtaques.style.display ='flex'
-
-    
-    
-    
+    imgPl.style.width='80px'
     
     if (imphipodo.checked){
         // alert('Seleccionaste a Hipodo')
         spnmascotapl.innerHTML='Hipodo'
+        //agregar imagen mokepon al apartado de resumen del
+        imgPl.src='./images/hipodo.png'
 
     }else if (impcapi.checked){
         //alert('Seleccionaste a Capi')
         spnmascotapl.innerHTML='Capi'
+        //agregar imagen mokepon al apartado de resumen del
+        imgPl.src='./images/capi.png'
+        
     }else if (imprat.checked){
         //alert('Seleccionaste a rat')
         spnmascotapl.innerHTML='Rat'
+        //agregar imagen mokepon al apartado de resumen del
+        imgPl.src='./images/rat.png'
     }else {
         alert('Debes seleccionar una mascota 😅')
         location.reload()
@@ -119,13 +126,21 @@ function selctMascotaPl(){
 
 function selctMascotaPc(){
     let selectAleatorio = aleatorio(1,3)
+    //agregar imagen mokepon al apartado de resumen del
+    imgpc.style.width='80px'
     
     if(selectAleatorio == 1) {
         spnmascotapc.innerHTML='Hipodo'
+        //agregar imagen mokepon al apartado de resumen del
+        imgpc.src='./images/hipodo.png'
     }else if(selectAleatorio == 2) {
         spnmascotapc.innerHTML='capi'
+        //agregar imagen mokepon al apartado de resumen del
+        imgpc.src='./images/capi.png'
     } else if(selectAleatorio == 3){
         spnmascotapc.innerHTML='Rat'
+        //agregar imagen mokepon al apartado de resumen del
+        imgpc.src='./images/rat.png'
     }
 }
 
